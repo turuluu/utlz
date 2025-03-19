8006af87ea7533ecc0d272c31c763effeb818190
Eczbek <eczbek.void@gmail.com>
2025-03-19 12:28:59 -0400

    Make preprocessor directive indentation consistent

--- src/defs.hpp
+++ src/defs.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #ifndef VAR_CAT3_
-    #define VAR_CAT3_(a, b, c) a##b##c
+#    define VAR_CAT3_(a, b, c) a##b##c
 #endif
 #ifndef VAR_CAT3
-    #define VAR_CAT3(a, b, c) VAR_CAT3_(a, b, c)
+#    define VAR_CAT3(a, b, c) VAR_CAT3_(a, b, c)
 #endif
