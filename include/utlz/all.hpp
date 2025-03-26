b63f5eb26c1512029c2fcedc06ad6fccd0276379
Joni Turunen <rojun.itu@gmail.com>
2025-03-26 20:12:39 +0200

    Move include file to own dir and add structure for inclusion all/individually

---
+++ include/utlz/all.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../../src/scoped.hpp"
+#include "../../src/arrays.hpp"
+#include "../../src/strings.hpp"
+#include "../../src/math.hpp"
--- src/utlz.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "../src/scoped.hpp"
-#include "../src/arrays.hpp"
-#include "../src/strings.hpp"
-#include "../src/math.hpp"
